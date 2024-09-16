dffd63ca8d12005a45b8d94619df55944b6b7e55
Andreea Mocan <andreeamocan89@gmail.com>
2024-09-16 12:23:51 +0200

    Added Romanian translations and updated language index

--- src/languages/index.js
+++ src/languages/index.js
@@ -1,11 +1,13 @@
 import tr from './tr.json';
 import en from './en.json';
 import de from './de.json';
+import ro from './ro.json';
 
-export const dictionaryList = { en, tr, de };
+export const dictionaryList = { en, tr, de, ro };
 
 export const languageOptions = {
   en: 'English',
   tr: 'Türkçe',
-  de: 'Deutsch'
+  de: 'Deutsch',
+  ro: 'Romana'
 };
